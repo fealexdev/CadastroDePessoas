1c95f6fc0dcc90b446231b5fbfdd0eedb81bb3c3
fealexdev <fe.alexandrecampos@gmail.com>
2022-08-05 10:05:32 -0300

    commit inicial

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Pessoa {
+
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private int idade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: "+codigo+ "" +
+                "\n"+ "Nome: "+nome+"" +
+                "\n"+"Endereço: "+endereco+"" +
+                "\n"+"Idade: "+idade+"\n";
+    }
+}
